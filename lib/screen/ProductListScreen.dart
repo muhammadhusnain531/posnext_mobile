5b1fe0de81ceada4d8fe308618fb3f8534d65178
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-10-19 17:01:05 +0500

    work on Product list  screen

--- lib/screen/ProductListScreen.dart
+++ lib/screen/ProductListScreen.dart
@@ -1,11 +1,28 @@
 
 import 'package:flutter/material.dart';
-
 class ProductListScreen extends StatelessWidget {
-  const ProductListScreen({super.key});
+  final List<Map<String, dynamic>> products = [
+    {'name': 'Product 1', 'barcode': '12345', 'price': 100.0},
+    {'name': 'Product 2', 'barcode': '67890', 'price': 150.0},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          var product = products[index];
+          return ListTile(
+            title: Text(product['name']),
+            subtitle: Text('Barcode: ${product['barcode']}'),
+            trailing: Text('Price: \PKR${product['price']}'),
+          );
+        },
+      ),
+    );
   }
 }

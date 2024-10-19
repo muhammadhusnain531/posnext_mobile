c5f0a7e99be0ac24d35c8e366a3b6ac1e94fc780
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-10-19 16:45:42 +0500

    work on sale return screen

--- lib/screen/ReturnScreen.dart
+++ lib/screen/ReturnScreen.dart
@@ -5,6 +5,19 @@ class ReturnScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Sale Return'),),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(10)),
+          TextField(
+            decoration: InputDecoration(label: Text('Enter Bill No'),
+                border: OutlineInputBorder(borderRadius:BorderRadius.circular(30.0))),
+          ),
+          Padding(padding: EdgeInsets.all(10))
+          ,ElevatedButton(onPressed: (){}, child: Text('Search'))
+        ],
+      ),
+    );
   }
 }
